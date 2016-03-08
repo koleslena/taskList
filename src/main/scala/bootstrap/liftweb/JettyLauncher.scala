770a005824417456039a2876f9eceee0161c3dd8
Helen Kolesnikova <koleslena@yandex.ru>
2016-03-08 16:59:55 +0600

    added procfile

--- src/main/scala/bootstrap/liftweb/JettyLauncher.scala
+++ src/main/scala/bootstrap/liftweb/JettyLauncher.scala
@@ -19,15 +19,8 @@ object JettyLancher extends App with Loggable{
 
     logger.info(s"port number is $port")
 
-/*    val webappDir: String = Option(this.getClass.getClassLoader.getResource("webapp"))
-      .map(_.toExternalForm)
-      .filter(_.contains("jar:file:")) // this is a hack to distinguish in-jar mode from "expanded"
-      .getOrElse("./target/webapp")
-
-    logger.info(s"webappDir: $webappDir")*/
-
     val server = new Server(port)
-    val context = new WebAppContext("src/webapp", Props.get("jetty.contextPath").openOr("/"))
+    val context = new WebAppContext("src/main/webapp", Props.get("jetty.contextPath").openOr("/"))
     server.setHandler(context)
     server.start()
     logger.info(s"Lift server started on port $port")
